fa44bac8a4d10ef5f04be110d03376a91c3ceedc
areumH <dkfmal0109@gmail.com>
2025-10-03 03:25:52 +0900

    feat: 헤더 컴포넌트 구현

---
+++ components/layout/Header.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Moon } from 'lucide-react';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header>
+      <div className='container mx-auto px-4 py-2.5 flex justify-between items-center border-b-1 border-b-gray-300'>
+        {/* 로고 */}
+        <Link href="/" className="font-bold text-xl">
+          <div>areumh.me</div>
+        </Link>
+
+        {/* 네비게이션 */}
+        <nav className="flex items-center gap-3 md:gap-6">
+          <Link href="/category" className="text-gray-800 hover:text-gray-500">
+            category
+          </Link>
+          <Link href="/portfolio" className="text-gray-800 hover:text-gray-500">
+            portfolio
+          </Link>
+          <button className="flex w-8 h-8 rounded-full justify-center items-center hover:bg-gray-300 cursor-pointer">
+            <Moon />
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+}
