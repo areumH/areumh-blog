3c0d301711a07882c4ae00a7bbcd4b0fc7e82e02
areumH <dkfmal0109@gmail.com>
2026-03-21 17:06:30 +0900

    fix: 모바일 환경 좌우 패딩 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
       <body className="flex flex-col min-h-screen">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
-          <main className="flex flex-col flex-grow w-full mx-auto px-7 md:px-20 py-5 md:py-8">{children}</main>
+          <main className="flex flex-col flex-grow w-full mx-auto px-6 md:px-20 py-5 md:py-8">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
